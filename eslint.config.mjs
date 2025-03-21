a214ca32a75220e28b6b8c88898232b5d309e778
Teklemuz Ayenew <111185891+Teklemuz@users.noreply.github.com>
2025-03-21 21:46:39 +0300

    eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,12 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off", // Disable no-explicit-any
-      "@typescript-eslint/no-unused-vars": "off", // Disable no-unused-vars
-    },
-  },
 ];
 
 export default eslintConfig;
